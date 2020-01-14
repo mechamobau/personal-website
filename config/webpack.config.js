96cb9314247e22380594a75b7b3635e389e4ed29
Lucas Viana <vianadossantoslucas@gmail.com>
2020-01-13 21:39:04 -0300

    :building_construction: adds module name resolution

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,6 +24,8 @@ const getClientEnvironment = require("./env");
 const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
 const ForkTsCheckerWebpackPlugin = require("react-dev-utils/ForkTsCheckerWebpackPlugin");
 const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
+const TsConfigPathsPlugin = require("awesome-typescript-loader")
+  .TsConfigPathsPlugin;
 
 const postcssNormalize = require("postcss-normalize");
 
@@ -312,7 +314,8 @@ module.exports = function(webpackEnv) {
         // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
         // please link the files into your node_modules/ and let module-resolution kick in.
         // Make sure your source files are compiled, as they will not be processed in any way.
-        new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])
+        new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+        new TsConfigPathsPlugin()
       ]
     },
     resolveLoader: {

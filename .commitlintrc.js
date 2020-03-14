1e89d285294dd6d4c14e164d996630c8a8d040e4
Lucas Viana <vianadossantoslucas@gmail.com>
2020-03-14 14:44:54 -0300

    :art: adds a linter to commits

---
+++ .commitlintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["./node_modules/commitlint-config-gitmoji"],
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 72],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"]
+    ],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."]
+  }
+};
